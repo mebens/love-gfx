e56b79cf0f1ce6aaa9dfe1491f2c359a2e53b484
Michael Ebens <michael@nova-fusion.com>
2012-12-31 12:03:24 +1100

    Fixed importation of non-existent files.

--- init.lua
+++ init.lua
@@ -1,6 +1,3 @@
 local path = ({...})[1]:gsub("%.init", "") 
 require(path .. ".postfx")
-require(path .. ".Spritemap")
-require(path .. ".Tilemap")
-require(path .. ".Grid")
 if ammo then require(path .. ".ammo") end
